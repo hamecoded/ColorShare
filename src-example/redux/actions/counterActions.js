4412bd7615f7fca5f05ae8c78490a908b8ebb8b1
Oded Sagir <oded.sagir@ridewithvia.com>
2018-02-07 19:18:43 +0200

    add combineReducer

--- src-example/redux/actions/counterActions.js
+++
@@ -1,9 +0,0 @@
-import * as actionTypes from '../actionTypes';
-
-export const add = () => ({
-  type: actionTypes.ADD
-});
-
-export const subtract = () => ({
-  type: actionTypes.SUB
-});
--- src-example/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-
-// reducers
-import rootReducer from './reducers/rootReducer';
-
-const store = createStore(rootReducer, {});
-
-export default store;
